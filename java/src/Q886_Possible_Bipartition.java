0914e8df2b59c7d8b82602f4cdfd3ae7a1b84f0e
yoguz <e.oguzhan.yildirim@gmail.com>
2020-05-31 21:15:47 +0300

    java | Q886

---
+++ java/src/Q886_Possible_Bipartition.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Q886_Possible_Bipartition {
+
+    ArrayList<Integer>[] graph;
+    Map<Integer, Integer> color;
+
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; ++i)
+            graph[i] = new ArrayList();
+
+        for (int[] edge: dislikes) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        color = new HashMap();
+        for (int node = 1; node <= N; ++node)
+            if (!color.containsKey(node) && !dfs(node, 0))
+                return false;
+        return true;
+    }
+
+    public boolean dfs(int node, int c) {
+        if (color.containsKey(node))
+            return color.get(node) == c;
+        color.put(node, c);
+
+        for (int nei: graph[node])
+            if (!dfs(nei, c ^ 1))
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Q886_Possible_Bipartition obj = new Q886_Possible_Bipartition();
+        System.out.println(obj.possibleBipartition(4, new int[][]{{1,2}, {1,3}, {2,4}}));
+    }
+}
