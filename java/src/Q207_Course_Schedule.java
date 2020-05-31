9b4068345f306fe107a6ea9156d158088b478535
yoguz <e.oguzhan.yildirim@gmail.com>
2020-05-31 21:15:06 +0300

    java | Q207

---
+++ java/src/Q207_Course_Schedule.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Q207_Course_Schedule {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+            graph[i] = new ArrayList();
+
+        boolean[] visited = new boolean[numCourses];
+        for(int i=0; i<prerequisites.length;i++){
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(!dfs(graph,visited,i))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(ArrayList[] graph, boolean[] visited, int course){
+        if(visited[course])
+            return false;
+        else
+            visited[course] = true;;
+
+        for(int i=0; i<graph[course].size();i++){
+            if(!dfs(graph,visited,(int)graph[course].get(i)))
+                return false;
+        }
+        visited[course] = false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Q207_Course_Schedule obj = new Q207_Course_Schedule();
+        System.out.println(obj.canFinish(2, new int[][]{{1,0}}));
+        System.out.println(obj.canFinish(2, new int[][]{{1,0}, {0,1}}));
+    }
+}
