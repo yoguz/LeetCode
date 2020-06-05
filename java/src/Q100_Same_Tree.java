93468e4e70911d66f0df5efdaac64f9cc6e420e3
yoguz <e.oguzhan.yildirim@gmail.com>
2020-06-05 22:11:31 +0300

    java | Q100

---
+++ java/src/Q100_Same_Tree.java
@@ -0,0 +1,20 @@
+import common.TreeNode;
+
+public class Q100_Same_Tree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (p != null && q == null))
+            return false;
+        else if (p == null && q == null)
+            return true;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        Q100_Same_Tree obj = new Q100_Same_Tree();
+        TreeNode root = TreeNode.createTree(new Integer[]{1,2,3});
+        System.out.println(obj.isSameTree(root, root));
+        TreeNode root2 = TreeNode.createTree(new Integer[]{1,3,2});
+        System.out.println(obj.isSameTree(root, root2));
+    }
+}
