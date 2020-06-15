88c497d0f2fae9abe9d1be4046fc2f5e36e1c5f5
yoguz <e.oguzhan.yildirim@gmail.com>
2020-06-15 20:51:12 +0300

    java | Q125

---
+++ java/src/Q125_Valid_Palindrome.java
@@ -0,0 +1,54 @@
+public class Q125_Valid_Palindrome {
+
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start <= end) {
+            while(start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+            while(start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            if(start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome_OnMemory(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+                sb.append((char)(s.charAt(i)-'A'+'a'));
+            else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
+                sb.append(s.charAt(i));
+            else if (s.charAt(i) >= '0' && s.charAt(i) <= '9')
+                sb.append(s.charAt(i));
+        }
+
+        s = sb.toString();
+        if (s.length() < 2)
+            return true;
+        int i = 0, j = s.length()-1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Q125_Valid_Palindrome obj = new Q125_Valid_Palindrome();
+        System.out.println(obj.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(obj.isPalindrome("race a car"));
+    }
+}
