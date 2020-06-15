643469be8278cbbdb02114374760bf87f5d0a481
yoguz <e.oguzhan.yildirim@gmail.com>
2020-06-15 20:52:48 +0300

    java | Q198

---
+++ java/src/Q198_House_Robber.java
@@ -0,0 +1,23 @@
+public class Q198_House_Robber {
+
+    public int rob(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        else if (nums.length == 1)
+            return nums[0];
+        else if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
+
+        nums[2] += nums[0];
+        for (int i = 3; i < nums.length; ++i) {
+            nums[i] += Math.max(nums[i-2], nums[i-3]);
+        }
+
+        return Math.max(nums[nums.length-1], nums[nums.length-2]);
+    }
+
+    public static void main(String[] args) {
+        Q198_House_Robber obj = new Q198_House_Robber();
+        System.out.println(obj.rob(new int[]{1,4,2,7,8,2,5,14,22,6,8,33,14,2,55}));
+    }
+}
