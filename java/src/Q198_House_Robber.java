93d32e872692e3afef02c8fb121baf95c4be2b0d
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-06-22 23:52:18 +0300

    java | Q198

--- java/src/Q198_House_Robber.java
+++ java/src/Q198_House_Robber.java
@@ -1,6 +1,6 @@
 public class Q198_House_Robber {
 
-    public int rob(int[] nums) {
+    public int robOld(int[] nums) {
         if (nums.length == 0)
             return 0;
         else if (nums.length == 1)
@@ -14,6 +14,18 @@ public class Q198_House_Robber {
         }
 
         return Math.max(nums[nums.length-1], nums[nums.length-2]);
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; ++i) {
+            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+        }
+        return nums[nums.length-1];
     }
 
     public static void main(String[] args) {
