69a28491323f74ee4e587188c00c2706e947d4ea
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-06-25 08:56:04 +0300

    java | Q39 again

--- java/src/Q39_Combination_Sum.java
+++ java/src/Q39_Combination_Sum.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Q39_Combination_Sum {
 
-    List<List<Integer>> res;
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        res = new ArrayList<>();
+    List<List<Integer>> resOld;
+    public List<List<Integer>> combinationSumOld(int[] candidates, int target) {
+        resOld = new ArrayList<>();
         Arrays.sort(candidates);
-        helper(new ArrayList<>(), candidates, 0, target, 0);
-        return res;
+        helperOld(new ArrayList<>(), candidates, 0, target, 0);
+        return resOld;
     }
 
-    private void helper(List<Integer> list, int[] candidates, int sum, int target, int startIdx) {
+    private void helperOld(List<Integer> list, int[] candidates, int sum, int target, int startIdx) {
         for (int i = startIdx; i < candidates.length; ++i){
             if (candidates[i] + sum > target)
                 break;
@@ -20,11 +21,36 @@ public class Q39_Combination_Sum {
             List<Integer> l2 = new ArrayList<>(list);
             l2.add(candidates[i]);
             if (candidates[i] + sum == target) {
-                res.add(l2);
+                resOld.add(l2);
                 break;
             } else {
-                helper(l2, candidates, candidates[i]+sum, target, i);
+                helperOld(l2, candidates, candidates[i]+sum, target, i);
             }
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+
+        List<List<Integer>> resList = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        helper(candidates, target, resList, list, 0);
+        return resList;
+    }
+
+    private void helper(int[] candidates, int target, List<List<Integer>> resList, List<Integer> list, int startIdx) {
+        if (target == 0) {
+            resList.add(list);
+            return;
+        }
+
+        for (int idx = startIdx; idx < candidates.length; ++idx) {
+            if (candidates[idx] > target)
+                return;
+
+            List<Integer> copyList = new LinkedList<>(list);
+            copyList.add(candidates[idx]);
+            helper(candidates, target-candidates[idx], resList, copyList, idx);
         }
     }
 
