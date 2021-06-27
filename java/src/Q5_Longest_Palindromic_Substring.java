748feb94433c2007fd4b99aa321134713ca8c611
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-06-27 11:00:34 +0300

    java | Q5 again

--- java/src/Q5_Longest_Palindromic_Substring.java
+++ java/src/Q5_Longest_Palindromic_Substring.java
@@ -1,6 +1,6 @@
 public class Q5_Longest_Palindromic_Substring {
 
-    public String longestPalindrome(String s) {
+    public String longestPalindromeOld(String s) {
         if (s.length() < 2)
             return s;
 
@@ -41,6 +41,51 @@ public class Q5_Longest_Palindromic_Substring {
         }
 
         return res;
+    }
+
+    public String longestPalindrome(String s) {
+        String longestOdd = "", longestEven = "";
+
+        int start = 0, end = 0, max = 1;
+
+        for (int i = 0; i < s.length(); ++i) {
+            int j = 1;
+            while (i - j >= 0 && i + j < s.length()) {
+                if (s.charAt(i-j) == s.charAt(i+j)) {
+                    if (2*j+1 > max) {
+                        max = 2*j+1;
+                        start = i-j;
+                        end = i+j;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+        }
+
+        longestOdd = s.substring(start, end+1);
+        start = 0; end = 0; max = 0;
+
+        for (int i = 0; i < s.length()-1; ++i) {
+            int j = 0;
+            while (i - j >= 0 && i + j + 1 < s.length()) {
+                if (s.charAt(i-j) == s.charAt(i+j+1)) {
+                    if (2*j+2 > max) {
+                        max = 2*j+2;
+                        start = i-j;
+                        end = i+j+1;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+        }
+
+        longestEven = s.substring(start, end+1);
+
+        return longestOdd.length() > longestEven.length() ? longestOdd : longestEven;
     }
 
     public static void main(String[] args) {
