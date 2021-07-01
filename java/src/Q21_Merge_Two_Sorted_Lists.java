2f125be32cead86d5b8087ed86bb42a3a3300549
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-07-01 14:14:41 +0300

    java | Q21 again

--- java/src/Q21_Merge_Two_Sorted_Lists.java
+++ java/src/Q21_Merge_Two_Sorted_Lists.java
@@ -2,7 +2,7 @@ import common.ListNode;
 
 public class Q21_Merge_Two_Sorted_Lists {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoListsOld(ListNode l1, ListNode l2) {
         if (l1 == null)
             return l2;
         if (l2 == null)
@@ -28,6 +28,34 @@ public class Q21_Merge_Two_Sorted_Lists {
             cur = cur.next;
         }
         cur.next = l1 == null? l2:l1;
+        return head;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        else if (l2 == null)
+            return l1;
+
+
+        ListNode head = l1.val <= l2.val ? l1 : l2;
+        ListNode other = head == l1 ? l2 : l1;
+
+        ListNode it = head, temp;
+
+        while (it.next != null) {
+            if (it.next.val <= other.val) {
+                it = it.next;
+            } else {
+                temp = it.next;
+                it.next = other;
+                it = other;
+                other = temp;
+            }
+        }
+
+        it.next = other;
+
         return head;
     }
 
