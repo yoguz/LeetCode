aac4fa04310e306f1701d3e105f383c89c8f48ee
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-07-03 10:16:53 +0300

    java | Q79 again

--- java/src/Q79_Word_Search.java
+++ java/src/Q79_Word_Search.java
@@ -33,7 +33,7 @@ public class Q79_Word_Search {
     }
 */
 
-    public boolean exist(char[][] board, String word) {
+    public boolean existOld(char[][] board, String word) {
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[0].length; ++j) {
                 if (helper(board, word, 0, i, j))
@@ -61,6 +61,34 @@ public class Q79_Word_Search {
                 || helper(board, word, wordIndex+1, i, j+1);
 
         board[i][j] = word.charAt(wordIndex);
+        return res;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[0].length; ++j) {
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int j, int idx) {
+        if (idx == word.length())
+            return true;
+
+        if (i < 0 || j < 0 || i == board.length || j == board[0].length || word.charAt(idx) != board[i][j])
+            return false;
+
+        board[i][j] = '-';
+        boolean res =  dfs(board, word, i, j+1, idx+1)
+                || dfs(board, word, i, j-1, idx+1)
+                || dfs(board, word, i+1, j, idx+1)
+                || dfs(board, word, i-1, j, idx+1);
+
+        board[i][j] = word.charAt(idx);
         return res;
     }
 
