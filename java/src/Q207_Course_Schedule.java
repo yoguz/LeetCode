ce8e695945734d889b69231cfa997855b423ed1b
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-07-04 09:47:07 +0300

    java | Q207

--- java/src/Q207_Course_Schedule.java
+++ java/src/Q207_Course_Schedule.java
@@ -1,36 +1,48 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Q207_Course_Schedule {
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList[] graph = new ArrayList[numCourses];
-        for(int i=0;i<numCourses;i++)
-            graph[i] = new ArrayList();
+    enum Color {
+        WHITE,
+        GRAY,
+        BLACK
+    };
 
-        boolean[] visited = new boolean[numCourses];
-        for(int i=0; i<prerequisites.length;i++){
-            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] dependants = new LinkedList[numCourses];
+        Color[] colors = new Color[numCourses];
+
+        for (int i = 0; i < numCourses; ++i) {
+            colors[i] = Color.WHITE;
+            dependants[i] = new LinkedList<>();
         }
 
-        for(int i=0; i<numCourses; i++){
-            if(!dfs(graph,visited,i))
+        for (int[] pre: prerequisites) {
+            dependants[pre[0]].add(pre[1]);
+        }
+
+        for (int i = 0; i < numCourses; ++i) {
+            if (colors[i] == Color.WHITE && isCyclic(dependants, colors, i))
                 return false;
         }
+
         return true;
     }
 
-    private boolean dfs(ArrayList[] graph, boolean[] visited, int course){
-        if(visited[course])
-            return false;
-        else
-            visited[course] = true;;
+    private boolean isCyclic(List<Integer>[] dependants, Color[] colors, int course){
+        colors[course] = Color.GRAY;
 
-        for(int i=0; i<graph[course].size();i++){
-            if(!dfs(graph,visited,(int)graph[course].get(i)))
-                return false;
+        for (Integer dependant: dependants[course]) {
+            if (colors[dependant] == Color.GRAY)
+                return true;
+
+            else if (colors[dependant] == Color.WHITE && isCyclic(dependants, colors, dependant))
+                return true;
         }
-        visited[course] = false;
-        return true;
+
+        colors[course] = Color.BLACK;
+        return false;
     }
 
     public static void main(String[] args) {
