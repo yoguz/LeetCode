9da173d091ddb6295c9afe20adaebfc22a585801
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-09-07 20:11:52 +0300

    cpp | Q124

---
+++ cpp/Q124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include "common.hpp"
+using namespace std;
+
+int maxVal;
+
+int maxPathSum(TreeNode* root) {
+        maxVal = root != 0 ? root->val : 0;
+        helper(root);
+        return maxVal;
+    }
+    
+    int helper(TreeNode* node) {
+        if (node == 0)
+        {
+            return 0;
+        }
+        
+        int left = helper(node->left);
+        int right = helper(node->right);
+        int localMax = max(node->val, max(node->val + left, node->val + right));
+        maxVal = max(maxVal, max(localMax, node->val + left + right));
+        return localMax;
+    }
+
+int main()
+{
+    TreeNode *root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+    cout << maxPathSum(root) << endl;
+
+    return 0;
+}
