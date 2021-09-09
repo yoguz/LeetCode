4ec8b209722fb252b5c21dc31158b9a50c3de208
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-09-09 08:55:27 +0300

    cpp | Q322

---
+++ cpp/Q322_Coin_Change.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int coinChange(vector<int> coins, int amount)
+{
+    int Max = amount + 1;
+    vector<int> dp(amount + 1, Max);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++)
+    {
+        for (int j = 0; j < coins.size(); j++)
+        {
+            if (coins[j] <= i)
+            {
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int main()
+{
+    cout << coinChange({1,2,5}, 11) << endl;
+
+    return 0;
+}
