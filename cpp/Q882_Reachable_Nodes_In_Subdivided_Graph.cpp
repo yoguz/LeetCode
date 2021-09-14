30209224a92db1bc6abcd166b9ec84465b635619
Esref Oguzhan Yildirim <e.oguzhan.yildirim@gmail.com>
2021-09-14 10:18:24 +0300

    cpp | Q882

---
+++ cpp/Q882_Reachable_Nodes_In_Subdivided_Graph.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int reachableNodes(vector<vector<int>> edges, int M, int N)
+{
+    unordered_map<int, unordered_map<int, int>> e;
+    for (auto v : edges)
+        e[v[0]][v[1]] = e[v[1]][v[0]] = v[2];
+        
+    priority_queue<pair<int, int>> pq;
+    pq.push({M, 0});
+    unordered_map<int, int> seen;
+    while (pq.size())
+    {
+        int moves = pq.top().first, i = pq.top().second;
+        pq.pop();
+        if (!seen.count(i))
+        {
+            seen[i] = moves;
+            for (auto j : e[i])
+            {
+                int moves2 = moves - j.second - 1;
+                if (!seen.count(j.first) && moves2 >= 0)
+                    pq.push({moves2, j.first});
+            }
+        }
+    }
+    int res = seen.size();
+    for (auto v : edges)
+    {
+        int a = seen.find(v[0]) == seen.end() ? 0 : seen[v[0]];
+        int b = seen.find(v[1]) == seen.end() ? 0 : seen[v[1]];
+        res += min(a + b, v[2]);
+    }
+    return res;
+}
+
+int main()
+{
+    cout << reachableNodes({{0, 1, 10}, {0, 2, 1}, {1, 2, 2}}, 6, 3) << endl;
+    cout << reachableNodes({{0, 1, 4}, {1, 2, 6}, {0, 2, 8}, {1, 3, 1}}, 10, 4) << endl;
+    return 0;
+}
